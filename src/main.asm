96b03900073c8fd6838828216d1bba79a1ab3aea
Les Harris <les@lesharris.com>
2021-04-05 17:52:26 -0700

    Basic hello world

--- src/main.asm
+++ src/main.asm
@@ -1,5 +1,39 @@
+.sdsctag 1.10, "smslib Hello World","smslib Hello World example based on Maxim's tutorial","lajohnston"
 .incdir "lib/smslib"
 .include "smslib.asm"
+.incdir "."
 
+.asciitable
+  map " " to "~" = 0
+.enda
+
+.incdir "assets"
+.section "assets" free
+  paletteData:
+    palette.rgb 0, 0, 0
+    palette.rgb 255, 255, 255
+
+  fontData:
+    .incbin "../assets/font.bin" fsize fontDataSize
+
+  message:
+    .asc "Hello, world!"
+    .db $ff
+.ends
+.incdir "."
+
+.section "init" free
 init:
-  jp init
+  palette.setSlot 0
+  palette.loadSlice paletteData, 2
+
+  patterns.setSlot 0
+  patterns.load fontData, fontDataSize
+
+  tilemap.setSlot 0, 0
+  tilemap.loadBytesUntil $ff message
+
+  vdp.enableDisplay
+
+  -: jr -
+.ends
